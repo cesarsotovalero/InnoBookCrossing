d191960550160d032ee0110f6af52fc787b6b75f
yadariya <66779337+yadariya@users.noreply.github.com>
2021-08-26 11:53:23 +0300

    Initial commit

---
+++ src/main/java/com/example/innobookcrossing/InnoBookCrossingApplication.java
@@ -0,0 +1,13 @@
+package com.example.innobookcrossing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoBookCrossingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnoBookCrossingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/innobookcrossing/InnoBookCrossingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.innobookcrossing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoBookCrossingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
