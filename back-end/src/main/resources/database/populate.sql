82116f61be47a19ee5e02cf3030d6ee1a7be1e51
yadariya <66779337+yadariya@users.noreply.github.com>
2021-09-25 15:57:12 +0300

    Data insertion fixed.

--- back-end/src/main/resources/database/populate.sql
+++ back-end/src/main/resources/database/populate.sql
@@ -1,70 +1,65 @@
 INSERT INTO users
-VALUES (1, '@vahitovadariya', '12345');
-INSERT INTO users
-VALUES (2, '@romanza705', '54321');
-INSERT INTO users
-VALUES (3, '@Kastrylia', '5678910');
+VALUES (1, '@vahitovadariya', '12345'),
+       (2, '@romanza705', '54321'),
+       (3, '@Kastrylia', '5678910');
 INSERT INTO book
 VALUES (1, 'War and peace', 'Leo Tolstoy', 'novel', '@vahitovadariya', 'very boring',
-        'https://cdn1.ozone.ru/s3/multimedia-o/wc1200/6046894776.jpg', 1);
-VALUES (2, 'Jonathan Livingston Seagull', 'Richard Bach', 'novella', '@vahitovadariya', 'self-help',
-        'https://en.wikipedia.org/wiki/Jonathan_Livingston_Seagull#/media/File:Johnathan_Livingston_Seagull.jpg', 1);
-VALUES (3, 'Atomic Habits', 'James Clear', 'motivation', '@vahitovadariya', 'very boring',
-        'https://images-na.ssl-images-amazon.com/images/I/51CPNIRSWeL._SX339_BO1,204,203,200_.jpg', 1);
-VALUES (4, 'Peace, Love, and Pasta', 'Scott Conant', 'kitchen book', '@vahitovadariya', 'cool',
-        'https://images-na.ssl-images-amazon.com/images/I/41ov3pzcvqL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg', 1);
-VALUES (5, 'Childhood', 'Leo Tolstoy', 'novel', '@vahitovadariya', 'interesting',
-        'https://m.media-amazon.com/images/I/41L-84BP-0L.jpg', 1);
-VALUES (6, 'Anna Karenina', 'Leo Tolstoy', 'novel', '@vahitovadariya', '',
-        'https://images-na.ssl-images-amazon.com/images/I/411PSSN77qL._SX312_BO1,204,203,200_.jpg', 1);
-VALUES (7, 'Greenlights', 'Matthew McConaughey', 'novel', '@vahitovadariya', '',
-        'https://m.media-amazon.com/images/I/41JMH+5JNCL.jpg', 1);
-VALUES (8, 'The Duel', 'Anton Chekhov', 'novel', '@vahitovadariya', '',
-        'https://m.media-amazon.com/images/I/41-7AQqPoBL.jpg', 1);
-VALUES (9, 'The Chorus Girl', 'Anton Chekhov', 'novel', '@vahitovadariya', '',
-        'https://m.media-amazon.com/images/I/51aKPWkxynL.jpg', 1);
-VALUES (10, 'Ward No. 6', 'Anton Chekhov', 'novel', '@vahitovadariya', '',
-        'https://m.media-amazon.com/images/I/41N9ERZtHvL.jpg', 1);
-
-VALUES (11, 'The Queen of Spades', 'Alexander Pushkin', 'novel', '@romanza705', '',
-        'https://m.media-amazon.com/images/I/51kk+XTS9fL.jpg', 2);
-VALUES (12, 'Eugene Onegin', 'Alexander Pushkin', 'novella', '@romanza705', '',
-        'https://images-na.ssl-images-amazon.com/images/I/51UPHwrigtL._SX342_SY445_QL70_FMwebp_.jpg', 2);
-VALUES (13, 'The Subtle Art of Not Giving a F*ck', 'Mark Manson', 'motivation', '@romanza705', 'v',
-        'https://m.media-amazon.com/images/I/51mN3bY0JjL.jpg', 2);
-VALUES (14, 'Cant Hurt Me', 'David Goggins', '', '@romanza705', '',
-        'https://m.media-amazon.com/images/I/41HXEG13EKL.jpg', 2);
-VALUES (15, 'Breaking the Habit of Being Yourself', 'Joe Dispenza', 'motivation', '@romanza705', 'g',
-        'https://m.media-amazon.com/images/I/41FyqtPwzZL.jpg', 2);
-VALUES (16, 'Quit Like a Woman', 'Holly Whitaker', '', '@romanza705', 'very boring',
-        'https://m.media-amazon.com/images/I/51raCv3WJVL.jpg', 2);
-VALUES (17, 'Elegantnye obekty', 'Yegor Bugaenko', '', '@romanza705', 'v',
-        'https://images-na.ssl-images-amazon.com/images/I/41ygWpL0qbL._SX355_BO1,204,203,200_.jpg', 2);
-VALUES (18, 'Do No Harm', 'Марш Генри', 'n', '@romanza705', 'ring',
-        'https://cdn1.ozone.ru/s3/multimedia-p/c360/6065973985.jpg', 2);
-VALUES (19, 'The Last Thing He Told Me', 'Laura Dave', 'novel', '@romanza705', 'very boring',
-        'https://m.media-amazon.com/images/I/51b-JoV-1xS.jpg', 2);
-VALUES (20, 'Apples Never Fall', 'Liane Moriarty', 'novel', '@romanza705', 'for alone women',
-        'https://m.media-amazon.com/images/I/41OFl6AFT6L.jpg', 2);
-
-
-VALUES (21, 'A Journey Into Front End Web Development', 'Shantell Barge', 'technical', '@Kastrylia', 'very boring',
-        'https://m.media-amazon.com/images/I/41wBgT-q4nL.jpg', 3);
-VALUES (22, 'Professional Front-end Architecture', 'Fabio Nolasco', 'technical', '@Kastrylia', 'self-help',
-        'https://m.media-amazon.com/images/I/71hL8m0otJL._AC_UY327_FMwebp_QL65_.jpg', 3);
-VALUES (23, 'Frontend Architecture for Design Systems', 'Micah Godbolt', 'technical', '@Kastrylia', ' ',
-        'https://images-na.ssl-images-amazon.com/images/I/51aeRzppjDL._SX342_SY445_QL70_FMwebp_.jpg', 3);
-VALUES (24, 'Learning Java', 'Marc Loy', 'technical', '@Kastrylia', ' \',
-        'https://m.media-amazon.com/images/I/91gRSdr8B2L._AC_UY327_FMwebp_QL65_.jpg', 3);
-VALUES (25, 'Java in a Nutshell', ' Benjamin J. Evans', 'technical', '@Kastrylia', 'very ',
-        'https://m.media-amazon.com/images/I/91s6pEQSOOL._AC_UY327_FMwebp_QL65_.jpg', 3);
-VALUES (26, 'Antoni: Let''s Do Dinner', 'Antoni Porowski', 'kitchen book', '@Kastrylia', 'very boring',
-        'https://images-na.ssl-images-amazon.com/images/I/71O2LBZj+uL._AC_SX75_CR,0,0,75,75_.jpg', 3);
-VALUES (27, 'How to Cook That: Crazy Sweet Creations', 'Ann Reardon', 'novel', '@Kastrylia', 'very boring',
-        'https://images-na.ssl-images-amazon.com/images/I/51zccXY937L._SX397_BO1,204,203,200_.jpg', 3);
-VALUES (28, 'The Nature of Nature', ' Enric Sala ', 'novel', '@Kastrylia', 'very ',
-        'https://m.media-amazon.com/images/I/91Sy3oWWAjL._AC_UY327_FMwebp_QL65_.jpg', 3);
-VALUES (29, 'The Self Help Book', 'Jared Graybeal', 'motivational', '@Kastrylia', ' ',
-        'https://images-na.ssl-images-amazon.com/images/I/41cC9bcI-kL._SX311_BO1,204,203,200_.jpg', 3);
-VALUES (30, 'Women of Means', 'Marlene Wagman-Geller', 'novel', '@Kastrylia', 'very boring',
+        'https://cdn1.ozone.ru/s3/multimedia-o/wc1200/6046894776.jpg', 1),
+       (2, 'Jonathan Livingston Seagull', 'Richard Bach', 'novella', '@vahitovadariya', 'self-help',
+        'https://en.wikipedia.org/wiki/Jonathan_Livingston_Seagull#/media/File:Johnathan_Livingston_Seagull.jpg', 1),
+       (3, 'Atomic Habits', 'James Clear', 'motivation', '@vahitovadariya', 'very boring',
+        'https://images-na.ssl-images-amazon.com/images/I/51CPNIRSWeL._SX339_BO1,204,203,200_.jpg', 1),
+       (4, 'Peace, Love, and Pasta', 'Scott Conant', 'kitchen book', '@vahitovadariya', 'cool',
+        'https://images-na.ssl-images-amazon.com/images/I/41ov3pzcvqL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg', 1),
+       (5, 'Childhood', 'Leo Tolstoy', 'novel', '@vahitovadariya', 'interesting',
+        'https://m.media-amazon.com/images/I/41L-84BP-0L.jpg', 1),
+       (6, 'Anna Karenina', 'Leo Tolstoy', 'novel', '@vahitovadariya', '',
+        'https://images-na.ssl-images-amazon.com/images/I/411PSSN77qL._SX312_BO1,204,203,200_.jpg', 1),
+       (7, 'Greenlights', 'Matthew McConaughey', 'novel', '@vahitovadariya', '',
+        'https://m.media-amazon.com/images/I/41JMH+5JNCL.jpg', 1),
+       (8, 'The Duel', 'Anton Chekhov', 'novel', '@vahitovadariya', '',
+        'https://m.media-amazon.com/images/I/41-7AQqPoBL.jpg', 1),
+       (9, 'The Chorus Girl', 'Anton Chekhov', 'novel', '@vahitovadariya', '',
+        'https://m.media-amazon.com/images/I/51aKPWkxynL.jpg', 1),
+       (10, 'Ward No. 6', 'Anton Chekhov', 'novel', '@vahitovadariya', '',
+        'https://m.media-amazon.com/images/I/41N9ERZtHvL.jpg', 1),
+       (11, 'The Queen of Spades', 'Alexander Pushkin', 'novel', '@romanza705', '',
+        'https://m.media-amazon.com/images/I/51kk+XTS9fL.jpg', 2),
+       (12, 'Eugene Onegin', 'Alexander Pushkin', 'novella', '@romanza705', '',
+        'https://images-na.ssl-images-amazon.com/images/I/51UPHwrigtL._SX342_SY445_QL70_FMwebp_.jpg', 2),
+       (13, 'The Subtle Art of Not Giving a F*ck', 'Mark Manson', 'motivation', '@romanza705', 'v',
+        'https://m.media-amazon.com/images/I/51mN3bY0JjL.jpg', 2),
+       (14, 'Cant Hurt Me', 'David Goggins', '', '@romanza705', '',
+        'https://m.media-amazon.com/images/I/41HXEG13EKL.jpg', 2),
+       (15, 'Breaking the Habit of Being Yourself', 'Joe Dispenza', 'motivation', '@romanza705', 'g',
+        'https://m.media-amazon.com/images/I/41FyqtPwzZL.jpg', 2),
+       (16, 'Quit Like a Woman', 'Holly Whitaker', '', '@romanza705', 'very boring',
+        'https://m.media-amazon.com/images/I/51raCv3WJVL.jpg', 2),
+       (17, 'Elegantnye obekty', 'Yegor Bugaenko', '', '@romanza705', 'v',
+        'https://images-na.ssl-images-amazon.com/images/I/41ygWpL0qbL._SX355_BO1,204,203,200_.jpg', 2),
+       (18, 'Do No Harm', 'Марш Генри', 'n', '@romanza705', 'ring',
+        'https://cdn1.ozone.ru/s3/multimedia-p/c360/6065973985.jpg', 2),
+       (19, 'The Last Thing He Told Me', 'Laura Dave', 'novel', '@romanza705', 'very boring',
+        'https://m.media-amazon.com/images/I/51b-JoV-1xS.jpg', 2),
+       (20, 'Apples Never Fall', 'Liane Moriarty', 'novel', '@romanza705', 'for alone women',
+        'https://m.media-amazon.com/images/I/41OFl6AFT6L.jpg', 2),
+       (21, 'A Journey Into Front End Web Development', 'Shantell Barge', 'technical', '@Kastrylia', 'very boring',
+        'https://m.media-amazon.com/images/I/41wBgT-q4nL.jpg', 3),
+       (22, 'Professional Front-end Architecture', 'Fabio Nolasco', 'technical', '@Kastrylia', 'self-help',
+        'https://m.media-amazon.com/images/I/71hL8m0otJL._AC_UY327_FMwebp_QL65_.jpg', 3),
+       (23, 'Frontend Architecture for Design Systems', 'Micah Godbolt', 'technical', '@Kastrylia', ' ',
+        'https://images-na.ssl-images-amazon.com/images/I/51aeRzppjDL._SX342_SY445_QL70_FMwebp_.jpg', 3),
+       (24, 'Learning Java', 'Marc Loy', 'technical', '@Kastrylia', ' \',
+        'https://m.media-amazon.com/images/I/91gRSdr8B2L._AC_UY327_FMwebp_QL65_.jpg', 3),
+       (25, 'Java in a Nutshell', ' Benjamin J. Evans', 'technical', '@Kastrylia', 'very ',
+        'https://m.media-amazon.com/images/I/91s6pEQSOOL._AC_UY327_FMwebp_QL65_.jpg', 3),
+       (26, 'Antoni: Let''s Do Dinner', 'Antoni Porowski', 'kitchen book', '@Kastrylia', 'very boring',
+        'https://images-na.ssl-images-amazon.com/images/I/71O2LBZj+uL._AC_SX75_CR,0,0,75,75_.jpg', 3),
+       (27, 'How to Cook That: Crazy Sweet Creations', 'Ann Reardon', 'novel', '@Kastrylia', 'very boring',
+        'https://images-na.ssl-images-amazon.com/images/I/51zccXY937L._SX397_BO1,204,203,200_.jpg', 3),
+       (28, 'The Nature of Nature', ' Enric Sala ', 'novel', '@Kastrylia', 'very ',
+        'https://m.media-amazon.com/images/I/91Sy3oWWAjL._AC_UY327_FMwebp_QL65_.jpg', 3),
+       (29, 'The Self Help Book', 'Jared Graybeal', 'motivational', '@Kastrylia', ' ',
+        'https://images-na.ssl-images-amazon.com/images/I/41cC9bcI-kL._SX311_BO1,204,203,200_.jpg', 3),
+       (30, 'Women of Means', 'Marlene Wagman-Geller', 'novel', '@Kastrylia', 'very boring',
         'https://images-na.ssl-images-amazon.com/images/I/51OXPEMqZBL._SX321_BO1,204,203,200_.jpg', 3);
