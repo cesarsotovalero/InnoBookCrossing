983b3d2e61e2a1dca344f38610fbfed745e26085
romanza705 <r.mukhtarov@innopolis.university>
2021-09-27 09:51:37 +0300

    Change position of carousel's label

--- front-end/src/bookViewers/carousel.js
+++ front-end/src/bookViewers/carousel.js
@@ -3,9 +3,14 @@ import Item from "./item.js"
 import "./carousel.css"
 
 export default function Carousel(props){
+    const styles ={
+        label:{
+            textAlign: 'center',
+        }
+    }
     return(
         <div>
-            <h1>{props.label}</h1>
+            <h1 style={styles.label}>{props.label}</h1>
             <ul className="carouselList">
                 { props.bookSet.map(book => {
                     return <li key={book.id}><Item book={book}/></li>
