b5f945fa59281c58951d169acca7a7823e931cb5
romanza705 <r.mukhtarov@innopolis.university>
2021-10-05 14:59:17 +0300

    Changes in global textarea styles, addBook block styles

--- front-end/src/mainComponents/addBook.js
+++ front-end/src/mainComponents/addBook.js
@@ -5,19 +5,22 @@ const styles ={
     block: {
         position: 'absolute',
         zIndex: '100',
-        width:'60%',
+        width:'30%',
         height:'450px',
-        backgroundColor:'#ccc',
+        backgroundColor:'#fff',
         display: 'block',
         left: '50%',
         transform: 'translate(-50%, 0)',
-        textAlign: 'center',
-        border: '3px solid dimgray',
-        borderRadius: '5px',
+        textAlign: 'left',
+        borderRadius: '25px',
+        boxShadow: '0 0 200px 100px #b0b3b2',
+        padding: '2%',
+        lineHeight: '0'
 
     },
     input: {
-        width: '60%',
+        marginBottom:'5px',
+        width: '96%',
     },
     button: {
     },
@@ -30,8 +33,9 @@ export default function AddBook(props){
             {props.style}
             <p>Enter book title</p><input style={styles.input}/>
             <p>Enter book author</p><input style={styles.input}/>
-            <p>Provide a link to book image</p><input style={styles.input}/>
-            <p>Enter book description</p><input style={styles.input}/>
+
+            <p>Provide a link to book image</p><input style={styles.input} type={'url'}/>
+            <p>Enter book description</p><textarea style={styles.input} rows={'4'}/>
             <p>Who are you?Tg alias</p><input style={styles.input}/>
             <button style={styles.button} className={'inactive_button'}>Submit book</button>
         </div>
