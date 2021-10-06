8ed15182821507db736269ed16d50d2b15cc5609
Dariya <66779337+yadariya@users.noreply.github.com>
2021-10-06 11:44:15 +0300

    Registration and signin fixed.

--- back-end/src/main/java/com/example/innobookcrossing/controller/AuthController.java
+++ back-end/src/main/java/com/example/innobookcrossing/controller/AuthController.java
@@ -3,13 +3,12 @@ package com.example.innobookcrossing.controller;
 import com.example.innobookcrossing.exceptions.AlreadyExistException;
 import com.example.innobookcrossing.jpa.UserRepository;
 import com.example.innobookcrossing.model.User;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class AuthController {
@@ -18,22 +17,24 @@ public class AuthController {
 
     @PostMapping("/signin")
     @CrossOrigin(origins = "http://localhost:3000")
-    public String singIn(@RequestBody List<String> userinfo) {
-        if (userRepository.findUserByAlias(userinfo.get(0)).getPassword().equals(userinfo.get(1))) {
-            return "Ok";
+    public String singIn(@RequestBody User user) throws NotFoundException {
+        if (userRepository.findUserByAlias(user.getAlias()) == null) {
+            throw new NotFoundException("User with such alias does not exist.");
         } else {
-            return "Not ok";
+            if (userRepository.findUserByAlias(user.getAlias()).getPassword().equals(user.getPassword())) {
+                return "User sing in successfully";
+            } else {
+                throw new NotFoundException("User with such password does not exist.");
+            }
         }
     }
 
     @PostMapping("/register")
     @CrossOrigin(origins = "http://localhost:3000")
     public User register(@RequestBody User user) {
-        return userRepository.save(user);
-//        if (!user.getAlias().equals(userRepository.findUserByAlias(user.getAlias()).getAlias()) &&
-//                !user.getPassword().equals(userRepository.findUserByAlias(user.getAlias()).getPassword()))
-//            return userRepository.save(user);
-//        else throw new AlreadyExistException("Such user already exists");
+        if (userRepository.findUserByAlias(user.getAlias()) == null) {
+            return userRepository.save(user);
+        } else throw new AlreadyExistException("User already exists.");
     }
 
 }
