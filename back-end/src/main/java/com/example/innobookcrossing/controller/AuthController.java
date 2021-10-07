56eadf232b9aeeffcf98ba490b0bb659f05bb71b
Dariya <66779337+yadariya@users.noreply.github.com>
2021-10-07 19:21:06 +0300

    Sign up returns id, alias

--- back-end/src/main/java/com/example/innobookcrossing/controller/AuthController.java
+++ back-end/src/main/java/com/example/innobookcrossing/controller/AuthController.java
@@ -9,26 +9,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
-
     @PostMapping("/signin")
     @CrossOrigin(origins = "http://localhost:3000")
-    public String singIn(@RequestBody User user) {
+    public Object singIn(@RequestBody User user) {
         if (userRepository.findUserByAlias(user.getAlias()) == null) {
             return "User with such alias does not exist.";
         } else {
             if (userRepository.findUserByAlias(user.getAlias()).getPassword().equals(user.getPassword())) {
-                return userRepository.findUserByAlias(user.getAlias()).getId().toString();
+                HashMap<String, String> userInfo = new HashMap<>();
+                userInfo.put("id:", userRepository.findUserByAlias(user.getAlias()).getId().toString());
+                userInfo.put("alias", userRepository.findUserByAlias(user.getAlias()).getAlias());
+                return userInfo;
             } else {
                 return "User with such password does not exist.";
             }
         }
     }
-
 
     @PostMapping("/register")
     @CrossOrigin(origins = "http://localhost:3000")
@@ -37,5 +40,4 @@ public class AuthController {
             return userRepository.save(user);
         } else throw new AlreadyExistException("User already exists.");
     }
-
 }
